0eba088f7ab5fa634facc61818b158257b1f328a
David Walsh <davidawalsh1984@gmail.com>
2023-05-20 13:15:14 +0100

    feat: create types

---
+++ src/types/product.ts
@@ -0,0 +1,19 @@
+export type Product = {
+  id: number;
+  slug: string;
+  title: string;
+  vendor: string;
+  tags: string[];
+  published: boolean;
+  url: string;
+  image_src: string;
+  option_value: string;
+  sku: string;
+  price: number;
+  subscription_discount: number | string;
+  subscription: boolean;
+};
+
+export type ProductsResponse = {
+  products: Product[];
+};
