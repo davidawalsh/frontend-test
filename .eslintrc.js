c648c644982d9fa17959596522ad75e3bd8e92f2
David Walsh <davidawalsh1984@gmail.com>
2023-05-20 12:56:50 +0100

    chore: convert to typescript

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
